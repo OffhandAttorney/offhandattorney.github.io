fbaafa0893006cffe0aaad312ac3df700044e7e1
Sean Kennedy <sean@splitleaf.net>
2016-02-11 12:54:58 -0500

    Update 2016-02-11T17:54:57.489Z

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 jQuery(function($) {
 
-  'use strict';
+    'use strict';
 
-  $('.fitvids').fitVids();
+    $('.fitvids').fitVids();
+
+    function artwork() {
+        var $pieces = $('#viewport.artwork > ul > li > a'),
+            $image = $('#viewport.artwork .image');
+        function set($l) {
+            var $img = $('<img>', {
+                src: $l.attr('href'),
+                alt: $l.attr('title')
+            });
+            $l.parent().siblings('li').removeClass('active');
+            $l.parent().addClass('active');
+            $image.html($img);
+        }
+        $pieces.on('click', function(e) {
+            e.preventDefault();
+            set($(this));
+        });
+        set($pieces.first());
+    }
+    artwork();
 
 });
